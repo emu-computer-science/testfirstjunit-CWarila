c81ded0b094ba82332b67734420b05ee38aef4a1
CWarila <cwarila@emich.edu>
2025-03-17 01:00:26 -0400

    Tests written.

---
+++ testingDates/DateSetDateTest.java
@@ -0,0 +1,14 @@
+package testingDates;
+
+public class DateSetDateTest {
+	
+	Date setter = new Date ("January", 1, 2000);
+	
+	@Test
+	public void testSetDay() {
+		setter.setDate("January", 20, 2000);
+		AssertEquals(setter, new Date("January", 1, 2000));
+		setter.setDate("January", 32, 2000);
+		AssertEquals(setter, new Date("January", 20, 2000););
+	}
+}
